c501052cb3f5663489989723ce2ac654bd0e8cc6
Joseph Kowalski <jkjr25@gmail.com>
2022-03-18 15:32:10 -0400

    Completed exercises

--- tree.py
+++ tree.py
@@ -2,22 +2,17 @@ import os
 
 
 def print_directory(file_path: str):
-    list_of_files = os.listdir(file_path)
-    file_list = list()
+    file_list = []
     file_open = open('directory_list.txt', 'w')
-    for file in list_of_files:
-        full_directory_path = os.path.join(file_path, file)
-        if os.path.isdir(full_directory_path):
-            file_list += print_directory(full_directory_path)
-        else:
-            file_list.append(full_directory_path)
+    for (dirpath, dirnames, filenames) in os.walk(file_path):
+        for file_name in filenames:
+            file_list.append(os.path.join(f'{dirpath}', f'{file_name}\n'))
     for file in file_list:
         file_open.write(file)
     file_open.close()
     with open('directory_list.txt', 'r') as file_handler:
         for line in file_handler:
-            print(f'{line}\n')
+            print(line)
 
 
-# print_directory('/Users/joe/PycharmProjects/Python8Ex')
-os.listdir('/Users/joe/PycharmProjects')
+print_directory('/Users/joe/PycharmProjects/Python8Ex')
